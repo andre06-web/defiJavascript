58804d7d50c097505e77ca7fc74c51cd4837e612
andre06-web <andyweb3000@gmail.com>
2021-06-16 13:51:44 +0200

    Exercice1 Defi Javascript

--- js/monscript.js
+++ js/monscript.js
@@ -1,1 +1,11 @@
-alert("ceci est le fichier Javascript");
+  function exo2()  {
+    
+    var nombre1 = 28 ;
+    var nombre2 = 64 ;
+    
+
+   document.getElementById("div2").innerHTML=nombre2;
+     
+}
+        
+ exo2();
