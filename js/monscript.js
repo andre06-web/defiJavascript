ede7872db49d2dd7e9d78d237730faf77665130e
andre06-web <andyweb3000@gmail.com>
2021-06-17 10:12:08 +0200

    Exercice 2

--- js/monscript.js
+++ js/monscript.js
@@ -1,11 +1,23 @@
-  function exo2()  {
-    
-    var nombre1 = 28 ;
-    var nombre2 = 64 ;
-    
+// Exercice 1 : Inverser les valeurs de deux variables
 
-   document.getElementById("div2").innerHTML=nombre2;
-     
-}
+function exo1(a,b) {
+     [a, b] = [b, a]; 
+     return [a,b];
+ }           
+            
+    var a = 25 ;
+    var b = 52 ;
+
+console.log("Exercice 1 :\n\n")
+ console.log('a : ' + a + ' b : ' + b);
+ var exo1Test = exo1(a,b); 
+console.log('a : ' + exo1Test[0] + ' b : ' + exo1Test[1]+"\n\n");   
+  
+console.log("Exercice 2 :\n\n")
+
+var Nom = "toto" ;
+var Age = 30;
+var Homme = true;
+ 
+console.log("Nom : "+Nom+" Age : "+Age+" Homme : "+Homme+"\n\n");
         
- exo2();
